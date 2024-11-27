c88af93b5c355b8f110cce87baa8f2c1d77a76e0
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2024-11-26 21:11:04 -0500

    feat: add labels and button

--- CodePathPrework/ViewController.swift
+++ CodePathPrework/ViewController.swift
@@ -15,5 +15,26 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func changeColors(_ sender: UIButton) {
+        let backgroundColor = changeColor()
+        view.backgroundColor = backgroundColor
+        
+        let textColor = changeColor()
+        name.textColor = textColor
+        school.textColor = textColor
+        job.textColor = textColor
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var school: UILabel!
+    @IBOutlet weak var job: UILabel!
 }
 
